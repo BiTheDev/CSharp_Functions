6a3a7ea3953859276cfeb6767ab46d8458ed1b40
BrianHo <Curry30@Brians-MacBook-Pro.local>
2018-10-01 16:08:41 -0700

    first push

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Functions
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // public static void SayHello(string name)
+            // {
+            //     System.Console.WriteLine($"Hello, {name}, how are you doing today?");
+            // }
+
+            // SayHello("Chee");
+
+
+            // public static void SayHello(string name = "buddy")
+            // {
+            //     Console.WriteLine($"Hey {name}");
+            // }
+
+             // We can call it without providing any arguments and the default value will be used...
+            //     SayHello();
+            // // ...or we can call it with an argument and that argument's value will be used
+            //     SayHello("Yoda");
+
+
+            // public static string SayHello(string name = "buddy")
+            // {
+            //     return $"Hey {name}";
+            // }
+            // string greeting;
+            // greeting = SayHello();
+            // Console.WriteLine(greeting);
+        }
+    }
+}
